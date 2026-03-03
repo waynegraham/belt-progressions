092c7104513549929910d9ebc213773afe5eaf00
Wayne Graham <wayne.graham@gmail.com>
2026-03-03 14:18:43 -0500

    update playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev -- --port 4173",
+    command: "pnpm run dev --port 4173",
     url: "http://127.0.0.1:4173",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
